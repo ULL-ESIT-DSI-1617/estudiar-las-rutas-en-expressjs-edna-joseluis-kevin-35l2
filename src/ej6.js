960416bc714c3a653a491bfc4778d7fd74f0d758
opelu <opelu8@gmail.com>
2017-03-02 17:56:41 +0000

    ej5.js realizado

---
+++ src/ej6.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+// simple logger for this router's requests
+// all requests to this router will first hit this middleware
+router.use(function(req, res, next) {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
+});
+
+// this will only be invoked if the path starts with /bar from the mount point
+router.use('/bar', function(req, res, next) {
+  // ... maybe some additional /bar logging ...
+  next();
+});
+
+// always invoked
+router.use(function(req, res, next) {
+  res.send('Hello World');
+});
+
+app.use('/foo', router);
+
+app.listen(8085, function () {
+  console.log("Servidor escuchando en el puerto: 8085");
+});
