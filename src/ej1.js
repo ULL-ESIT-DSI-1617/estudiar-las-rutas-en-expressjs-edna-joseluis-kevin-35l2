57b8a4d9c9a895189984c37b804aa2e190688aa2
alu0100821390 <alu0100821390@ull.edu.es>
2017-03-02 17:18:07 +0000

    Modificar ej1.js

--- src/ej1.js
+++ src/ej1.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-  res.download('../docs/CAPITULO1/CAPITULO1.md');
 });
 
 app.post('/', function (req, res) {
