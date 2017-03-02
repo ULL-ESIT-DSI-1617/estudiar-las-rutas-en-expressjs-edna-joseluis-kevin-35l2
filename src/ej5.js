5219a98d17bd16ac4e862c8c377537b2b14fd467
opelu <opelu8@gmail.com>
2017-03-02 17:52:39 +0000

    ej4.js realizado

---
+++ src/ej5.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+// customizing the behavior of router.param()
+router.param(function(param, option) {
+  return function (req, res, next, val) {
+    if (val == option) {
+      next();
+    }
+    else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+// using the customized router.param()
+router.param('id', 1337);
+
+// route to trigger the capture
+router.get('/user/:id', function (req, res) {
+  res.send('OK');
+});
+
+app.use(router);
+
+app.listen(8084, function () {
+  console.log("Servidor escuchando en el puerto: 8084");
+});
