4f892fd6c01c49b3170b698e03cfd8b5aa069a23
opelu <opelu8@gmail.com>
2017-03-02 17:25:32 +0000

    ej2.js añadiendo manipuladores de ruta

--- src/ej2.js
+++ src/ej2.js
@@ -29,6 +29,54 @@ app.get(/.*fly$/, function(req, res) {
   res.send('/.*fly$/');
 });
 
+app.get('/example/a', function (req, res) {
+  res.send('Hello from A!');
+});
+
+
+app.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from B!');
+});
+
+
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+};
+
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+};
+
+var cb2 = function (req, res) {
+  res.send('Hello from C!');
+};
+
+app.get('/example/c', [cb0, cb1, cb2]);
+
+
+var db0 = function (req, res, next) {
+  console.log('DB0');
+  next();
+};
+
+var db1 = function (req, res, next) {
+  console.log('DB1');
+  next();
+};
+
+app.get('/example/d', [db0, db1], function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from D!');
+});
+
+
 app.listen(8081, function() {
     console.log("Servidor escuchando en el puerto: 8081");
 });
