919a5a609943b7bb7d353b8350d56b8ee10e1087
Edna Galiano <ednagc.11@gmail.com>
2017-03-02 20:28:12 +0000

    Añadiendo tarea ej-2 y corriginedo ej-1

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,9 +2,16 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var ghPages = require('gulp-gh-pages');
 
-// Tarea ej-1-------------------------------------------------------
+// Tarea ej-1 -------------------------------------------------------
 
 
 gulp.task('ej-1', function() {
-  return gulp.src('').pipe(shell(['./src/generate-gitbook']));
+  return gulp.src('').pipe(shell(['./src/ej1']));
+});
+
+// Tarea ej-2 -------------------------------------------------------
+
+
+gulp.task('ej-1', function() {
+  return gulp.src('').pipe(shell(['./src/ej2']));
 });
