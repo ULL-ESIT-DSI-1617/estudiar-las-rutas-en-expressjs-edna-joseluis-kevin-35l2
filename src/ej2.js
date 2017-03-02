47c76c28df7a2a4c26a1d09bfcb204cca6cc89aa
opelu <opelu8@gmail.com>
2017-03-02 17:01:21 +0000

    ej2.js realizado v1.0.1

--- src/ej2.js
+++ src/ej2.js
@@ -19,3 +19,7 @@ app.get('/ab*cd', function(req, res) {
 app.get('/ab(cd)?e', function(req, res) {
  res.send('ab(cd)?e');
 });
+
+app.listen(8081, function() {
+    console.log("Servidor escuchando en el puerto: 8081");
+});
