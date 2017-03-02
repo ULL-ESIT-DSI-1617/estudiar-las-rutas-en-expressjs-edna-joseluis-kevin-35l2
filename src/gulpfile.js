5edb27e4d88202b58dfaee68c3e54d41718e6a5c
Edna Galiano <ednagc.11@gmail.com>
2017-03-02 20:30:50 +0000

    Añadiendo tarea ej-4

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,21 +4,27 @@ var ghPages = require('gulp-gh-pages');
 
 // Tarea ej-1 -------------------------------------------------------
 
-
 gulp.task('ej-1', function() {
   return gulp.src('').pipe(shell(['./src/ej1']));
 });
 
-// Tarea ej-2 -------------------------------------------------------
 
+// Tarea ej-2 -------------------------------------------------------
 
 gulp.task('ej-1', function() {
   return gulp.src('').pipe(shell(['./src/ej2']));
 });
 
-// Tarea ej-2 -------------------------------------------------------
 
+// Tarea ej-3 -------------------------------------------------------
 
 gulp.task('ej-3', function() {
   return gulp.src('').pipe(shell(['./src/ej3']));
+});
+
+
+// Tarea ej-4 -------------------------------------------------------
+
+gulp.task('ej-4', function() {
+  return gulp.src('').pipe(shell(['./src/ej4']));
 });
