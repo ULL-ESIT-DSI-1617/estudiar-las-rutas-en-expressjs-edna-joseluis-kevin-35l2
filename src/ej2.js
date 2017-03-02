f55cd3e104944b366801123b7670e0668ede5760
opelu <opelu8@gmail.com>
2017-03-02 17:20:27 +0000

    ej2.js añadiendo exp.regulares

--- src/ej2.js
+++ src/ej2.js
@@ -20,6 +20,15 @@ app.get('/ab(cd)?e', function(req, res) {
  res.send('ab(cd)?e');
 });
 
+app.get(/a/, function(req, res) {
+  res.send('/a/');
+});
+
+
+app.get(/.*fly$/, function(req, res) {
+  res.send('/.*fly$/');
+});
+
 app.listen(8081, function() {
     console.log("Servidor escuchando en el puerto: 8081");
 });
