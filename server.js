a599eb0f7694e5d97c07e0bf7dff9b639faef954
alu0100821390 <alu0100821390@ull.edu.es>
2017-03-03 13:03:39 +0000

    Desplegar libro en gh-pages

---
+++ server.js
@@ -0,0 +1,20 @@
+// Importar
+var express = require('express');
+
+// instanciar
+var app = express();
+app.set('port', (process.env.PORT || 8081));
+
+
+// Ruteo
+app.use(express.static('./gh-pages/'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+
+// Escuchar
+var server = app.listen(app.get('port'), function () {
+    console.log('Servidor escuchando en el puerto %s', app.get('port'));
+});
